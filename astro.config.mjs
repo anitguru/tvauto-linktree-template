029da56bfd50d6a4fea0a508fae098249b028e6f
anitguru <me@anit.guru>
2025-01-24 22:13:16 -0500

    Added compression on build option.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,12 @@ import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), compress()],
+  integrations: [
+    tailwind(),
+    compress({
+      gzip: true, // Ensure gzip is enabled
+      brotli: true, // Optionally enable Brotli
+      // You can add more options if needed
+    }),
+  ],
 });
